1ca9c7766345a3996c4497870f228a21897aa899
Nick Semenkovich <nick@calnexin.com>
2010-06-15 13:48:38 -0500

    Initial scan commit.

---
+++ scan.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2010 Nick Semenkovich <semenko@alum.mit.edu>
+#
+# This software is released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
+#
+
+""" Invokes RNAhybrid, passes to TBA. """
+
+
+# Binary Paths (Unnecessary if the execuatbles are in your $PATH)
+
+RNAHYBRID_PATH = 'rnahybrid/src/RNAhybrid'
+TBA_PATH = 'tba'
+
+# Database Settings
+
+import MySQLdb
+import os
+import subprocess
+import sys
+import time
+from optparse import OptionParser, OptionGroup
+
+
+def rnahybrid():
+    """ Execute RNAhybrid. """
+
+    # TODO: Allow user to change flags to RNAhybrid
+    str = "-c -s 3utr_human -t target -q query"
+
+    conn = MySQLdb.connect(host = "localhost",
+                           user = "root",
+                           passwd = "KleinersLaws",
+                           db = "nagarajan")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT UTR_SEQ FROM T_PRM_UTRS_MIRTARGET WHERE UTR_COORDINATES REGEXP \"^[0-9]*\_[0-9]*$\" LIMIT 1")
+    # output looks like:
+    # command_line:1957:command_line:9:-21.6:0.597445:966:A       G U: ACCCCGG G : UGGGGCC C :        G
+    
+    while True:
+        val = cursor.fetchone()
+        if val == None:
+            break
+        process = subprocess.Popen([RNAHYBRID_PATH, '-c', '-p', '0.8', '-s', '3utr_human', val[0], 'TGGGGCCGC'],
+                                   shell=False,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        stdoutdata, stderrdata = process.communicate()
+        print stdoutdata
+
+
+    if process.returncode != 0:
+        raise Exception('An error occurred in executing RNAhybrid.')
+    
+    pass
+
+def tba():
+    """ Execute TBA. """
+
+    pass
+
+def main():
+    """ Main execution. """
+
+    # TODO: Add some assertion error checking?
+
+    usage = "usage: %prog [OPTIONS]"
+    parser = OptionParser(usage)
+
+    # General settings
+    parser.add_option("-j", help="Threads. We perform pseudo-multithreading by parallelizing the "
+                      "invocations of TBA. This is basically a spawned process limit. [Default: 2]",
+                      default=2, action="store", type="int", dest="threads")
+
+    # RNAhybrid Options
+    group = OptionGroup(parser, "RNAhybrid Settings (optional)")
+
+    group.add_option("-m", "--longstring", help="Helpstring",
+                     default=False, action="store_true", dest="variableAbc")
+
+    parser.add_option_group(group)
+
+    
+    (options, args) = parser.parse_args()
+    if len(args) == 0:
+        parser.error("Try -h for help.")
+
+    rnahybrid()
+
+            
+if __name__ == "__main__":
+    main()
